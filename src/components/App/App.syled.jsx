dcddde7276584b24e8bf2fc332f459f0bd0508cc
Maksym <maxthebest1906@gmail.com>
2023-02-09 23:07:25 +0200

    styles fix

--- src/components/App/App.syled.jsx
+++ src/components/App/App.syled.jsx
@@ -22,34 +22,6 @@ export const ErrorMessage = styled.p`
     border: ${props => props.theme.colors.border};
 `;
 
-export const Toggle = styled.button`
-    display: inline-block;
-    width: 50px;
-    height: 30px;
-    background: transparrent;
-    mix-blend-mode: soft-light;
-    border: ${props => props.theme.colors.border};
-    border-radius: 50%;
-    position: relative;
-    &::before{
-        content: 'on';
-        width: 30px;
-        height: 30px;
-        background: background: ${props => props.theme.colors.cardBgColor};
-        position: absolute;
-        top: 50%;
-        left: 0;
-        right: auto;
-        transform: translateY(-50%);
-        transition: all 250ms linear;
-    }
-    &.off::before{
-        content: 'off';
-        left: auto;
-        right: 0;
-    }
-`;
-
 export const MovieBottom = styled.div`
     margin: 40px 0 0 0;
 `;
@@ -59,6 +31,12 @@ export const MovieControls = styled.div`
     align-items: flex-start;
     justify-content: space-between;
     padding: 0 15px;
+    @media screen and (max-width: 420px) {
+        flex-direction: column;
+        &>div{
+            margin-top: 10px;
+        }
+    }
     &>div{
         &>a{
             padding: 8px 16px;
